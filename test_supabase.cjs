f417aff8588c2dc864254606a6d56c830caac127
stationzizaindorrij-gif <stationzizaindorrij@gmail.com>
2026-07-13 14:17:59 +0100

    Delete test_supabase.cjs

--- test_supabase.cjs
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
-const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_ANON_KEY);
-
-async function test() {
-  const { data: { user }, error: authErr } = await supabase.auth.getUser(); // This will fail without token. But we don't have auth here. Wait, we can't test without user session!
-}
-test();
