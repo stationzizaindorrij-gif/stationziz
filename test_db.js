37218283eefc9d691e9c296b05b8555c14c043d9
stationzizaindorrij-gif <stationzizaindorrij@gmail.com>
2026-07-13 14:18:07 +0100

    Delete test_db.js

--- test_db.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.VITE_SUPABASE_URL || 'YOUR_SUPABASE_URL';
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY || 'YOUR_SUPABASE_KEY';
-
-// We can just check the schema if we have the types, or we can use the environment variables
